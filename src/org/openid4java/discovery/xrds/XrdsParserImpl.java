4421e68774414392bcb3ca84a0d337c7f1cb26da
Chris Hall <chris@janrain.com>
2013-01-31 18:09:08 -0800

    squash

--- src/org/openid4java/discovery/xrds/XrdsParserImpl.java
+++ src/org/openid4java/discovery/xrds/XrdsParserImpl.java
@@ -155,7 +155,7 @@ public class XrdsParserImpl implements XrdsParser
     private Document parseXmlInput(String input) throws DiscoveryException
     {
 
-        String regex = "<Expires>\\d{4}-\\d{2}-\\d{2}T()\\d:\\d{2}:\\d{2}Z<\\/Expires>";
+        String regex = ".+<Expires>\\d{4}-\\d{2}-\\d{2}T()\\d:\\d{2}:\\d{2}Z<\\/Expires>.+";
         if (input.matches(regex))
         {
             input = input.replaceAll(regex, "0");
